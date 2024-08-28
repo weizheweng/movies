12566e1d9be5f509a28050e27aad3c783ee31bf0
wei-zhe <weizheweng@google.com>
2024-08-29 00:00:26 +0800

    feat: new Component MovieSynopsisModal

--- src/components/Movies/MovieListCard.tsx
+++ src/components/Movies/MovieListCard.tsx
@@ -1,9 +1,11 @@
-import { Box, Text, Image, VStack, Tag, TagLeftIcon, HStack, AspectRatio } from '@chakra-ui/react'
+import { Box, Text, Image, VStack, Tag, TagLeftIcon, HStack, AspectRatio, Icon, TagLabel, Flex, useDisclosure } from '@chakra-ui/react'
 import { type Movie } from '../../types/Movies'
 import { MOVIE_IMAGE_BASE_URL } from '../../constants/constants'
-import { StarIcon } from '@chakra-ui/icons'
+import { InfoIcon, StarIcon } from '@chakra-ui/icons'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { colors } from '../../styles/colors'
+import { MovieSynopsisModal } from './MovieSynopsisModal'
 
 interface MoviesListCardProps {
   data: Movie,
@@ -23,35 +25,48 @@ export function MovieListCard ({ data }: MoviesListCardProps) {
   const { title, poster_path: posterPath } = data
   const imagePath = `${MOVIE_IMAGE_BASE_URL}${posterPath}`
   const [isLoad, setIsLoad] = useState(false)
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    <Box sx={cardStyle} _hover={{ opacity: 0.6 }} onClick={() => navigate(`/movies/${data.id}`)}>
-      <AspectRatio ratio={2 / 3}>
-        <Image
-          src={imagePath}
-          alt={title}
-          width="100%"
-          bgColor="gray.300"
-          borderTopRadius={12}
-          opacity={isLoad ? 1 : 0}
-          transition="opacity 0.6s ease-in-out"
-          onLoad={() => setIsLoad(true)}
-        />
-      </AspectRatio>
-      <VStack p={2} pt={3} gap={1.5} alignItems="flex-start">
-        <HStack gap={2}>
-          <Tag>
-            <TagLeftIcon as={StarIcon} color="yellow.500" />
-            {(data.vote_average).toFixed(1)}
-          </Tag>
-          <Tag>
-            {data.original_language.toUpperCase()}
-          </Tag>
-        </HStack>
-        <Text fontSize="md" fontWeight="bold" noOfLines={1}>
-          {title}
-        </Text>
-      </VStack>
-    </Box>
+    <>
+      <Box sx={cardStyle}>
+        <AspectRatio ratio={2 / 3} _hover={{ opacity: 0.6 }} onClick={() => navigate(`/movies/${data.id}`)}>
+          <Image
+            src={imagePath}
+            alt={title}
+            width="100%"
+            bgColor="gray.300"
+            borderTopRadius={12}
+            opacity={isLoad ? 1 : 0}
+            transition="opacity 0.6s ease-in-out"
+            onLoad={() => setIsLoad(true)}
+          />
+        </AspectRatio>
+        <VStack p={2} pt={3} gap={1.5} alignItems="flex-start">
+          <HStack gap={1} justifyContent="space-between" width="100%">
+            <Flex gap={1}>
+              <Tag gap={1} backgroundColor={colors.gray['300']}>
+                <TagLeftIcon as={StarIcon} color="yellow.500" mr={0} />
+                <TagLabel color={colors.gray['800']}>
+                  {(data.vote_average).toFixed(1)}
+                </TagLabel>
+              </Tag>
+              <Tag backgroundColor={colors.gray['300']}>
+                <TagLabel color={colors.gray['800']}>
+                  {data.original_language.toUpperCase()}
+                </TagLabel>
+              </Tag>
+            </Flex>
+            <Icon as={InfoIcon} boxSize={5} _hover={{ opacity: 0.6 }} onClick={onOpen} />
+          </HStack>
+          <Text fontSize="md" fontWeight="bold" noOfLines={1} _hover={{ opacity: 0.6 }} onClick={() => navigate(`/movies/${data.id}`)}>
+            {title}
+          </Text>
+        </VStack>
+      </Box>
+      {
+        isOpen && <MovieSynopsisModal isOpen={isOpen} onClose={onClose} movieId={String(data.id)} />
+      }
+    </>
   )
 }
