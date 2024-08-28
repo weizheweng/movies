e5175691acb490081d0de63c88260c64c096a99c
wei-zhe <weizheweng@google.com>
2024-08-29 01:05:27 +0800

    feat: add Spinner in MovieSynopsisModal

--- src/components/Movies/MovieSynopsisModal.tsx
+++ src/components/Movies/MovieSynopsisModal.tsx
@@ -1,11 +1,10 @@
-import { AspectRatio, Image, Grid, GridItem, HStack, Modal, ModalBody, ModalContent, ModalOverlay, VStack } from '@chakra-ui/react'
+import { AspectRatio, Image, Grid, GridItem, Modal, ModalBody, ModalContent, ModalOverlay, VStack, Spinner, Flex } from '@chakra-ui/react'
 import { useMovieDetails } from '../../hooks-api/useMovieDetails'
 import { useState } from 'react'
 import { MOVIE_IMAGE_BASE_URL } from '../../constants/constants'
 import { MovieTitle } from '../MovieDetails/MovieTitle'
 import { MovieGenres } from '../MovieDetails/MovieGenres'
 import { MovieOverview } from '../MovieDetails/MovieOverview'
-import { MovieLink } from '../MovieDetails/MovieLink'
 import { MovieVote } from '../MovieDetails/MovieVote'
 
 interface MovieSynopsisModalProps {
@@ -16,7 +15,7 @@ interface MovieSynopsisModalProps {
 
 export function MovieSynopsisModal ({ isOpen, onClose, movieId }: MovieSynopsisModalProps) {
   const [isLoad, setIsLoad] = useState(false)
-  const { data: movieDetailSData } = useMovieDetails(movieId)
+  const { data: movieDetailSData, isLoading } = useMovieDetails(movieId)
   const {
     title,
     poster_path: posterPath,
@@ -30,35 +29,38 @@ export function MovieSynopsisModal ({ isOpen, onClose, movieId }: MovieSynopsisM
       <ModalOverlay />
       <ModalContent>
         <ModalBody p={4}>
-          <VStack gap={4} width="100%">
-            <Grid templateColumns="repeat(12, 1fr)" gap={4} width="100%">
-              <GridItem colSpan={4}>
-                <AspectRatio ratio={2 / 3}>
-                  <Image
-                    src={imagePath}
-                    alt={title}
-                    bgColor="gray.300"
-                    opacity={isLoad ? 1 : 0}
-                    transition="opacity 0.6s ease-in-out"
-                    onLoad={() => setIsLoad(true)}
-                    borderRadius={12}
-                    boxShadow="2px 2px 4px 2px rgba(0, 0, 0, 0.1)"
-                  />
-                </AspectRatio>
-              </GridItem>
-              <GridItem colSpan={8}>
-                <VStack gap={2} height="100%" justifyContent="space-between">
-                  <MovieTitle title={title} originalTitle={originalTitle} releaseDate={releaseDate} runtime={runtime} isHorizontalAligned={false} />
-                  <MovieGenres movieId={movieId} />
-                  <HStack justifyContent="flex-start" width="100%">
-                    <MovieLink movieId={movieId} />
-                    <MovieVote movieId={movieId} />
-                  </HStack>
-                </VStack>
-              </GridItem>
-            </Grid>
-            <MovieOverview movieId={movieId} />
-          </VStack>
+          {
+            !isLoading
+              ? <VStack gap={4} width="100%">
+                <Grid templateColumns="repeat(12, 1fr)" gap={4} width="100%">
+                  <GridItem colSpan={4}>
+                    <AspectRatio ratio={2 / 3}>
+                      <Image
+                        src={imagePath}
+                        alt={title}
+                        bgColor="gray.300"
+                        opacity={isLoad ? 1 : 0}
+                        transition="opacity 0.6s ease-in-out"
+                        onLoad={() => setIsLoad(true)}
+                        borderRadius={12}
+                        boxShadow="2px 2px 4px 2px rgba(0, 0, 0, 0.1)"
+                      />
+                    </AspectRatio>
+                  </GridItem>
+                  <GridItem colSpan={8}>
+                    <VStack gap={2} height="100%" alignItems="flex-start">
+                      <MovieTitle title={title} originalTitle={originalTitle} releaseDate={releaseDate} runtime={runtime} isHorizontalAligned={false} />
+                      <MovieGenres movieId={movieId} />
+                      <MovieVote movieId={movieId} />
+                    </VStack>
+                  </GridItem>
+                </Grid>
+                <MovieOverview movieId={movieId} noOfLines={6} />
+              </VStack>
+              : <Flex gap={4} alignItems="center" justifyContent="center" minH={200}>
+                <Spinner size="xl" />
+              </Flex>
+          }
         </ModalBody>
       </ModalContent>
     </Modal>
