15db7b92af92328d2bef063a13c28b0267857f6b
wei-zhe <weizheweng@google.com>
2024-09-01 23:44:00 +0800

    fix: type error in useMovieSimilar.ts

--- src/hooks-api/useMovieSimilar.ts
+++ src/hooks-api/useMovieSimilar.ts
@@ -2,11 +2,11 @@ import useSWR from 'swr'
 import { urlWithQueryParams } from '../utils/urlWithQueryParams'
 import { tmdbSWRFetcher } from '../utils/tmdbSWRFetcher'
 import { LANGUAGE } from '../constants/languageEnum'
-import { type MovieList } from '../types/Movies'
+import { type MovieListResponse } from '../types/Movies'
 
 export function useMovieSimilar (movieId?: string) {
   const key = urlWithQueryParams(`/movie/${movieId}/similar`, { language: LANGUAGE.ZH_TW })
-  const { data, error, isLoading, mutate } = useSWR<MovieList>(key, tmdbSWRFetcher)
+  const { data, error, isLoading, mutate } = useSWR<MovieListResponse>(key, tmdbSWRFetcher)
 
   return {
     data,
