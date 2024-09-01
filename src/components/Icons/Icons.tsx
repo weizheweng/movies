3beec4f3baddc3d40a94fd7d7e16a0121c572dc7
wei-zhe <weizheweng@google.com>
2024-09-01 20:25:26 +0800

    feat: new icon and svg

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,5 +1,13 @@
+import ArrowLeftIcon from '@assets/arrow-left.svg?react'
+import ArrowRightIcon from '@assets/arrow-right.svg?react'
+import DoubleArrowLeftIcon from '@assets/double-arrow-left.svg?react'
+import DoubleArrowRightIcon from '@assets/double-arrow-right.svg?react'
 import LogoIcon from '@assets/movies.svg?react'
 import IMDbIcon from '@assets/imdb.svg?react'
 
+export const ArrowLeft = ArrowLeftIcon
+export const ArrowRight = ArrowRightIcon
+export const DoubleArrowLeft = DoubleArrowLeftIcon
+export const DoubleArrowRight = DoubleArrowRightIcon
 export const Logo = LogoIcon
 export const IMDb = IMDbIcon
